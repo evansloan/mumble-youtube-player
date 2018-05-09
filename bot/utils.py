0b9e41db2a2334446dfe54401ecf1b54898ea9d4
Evan Sloan <evansloan082@gmail.com>
2018-05-08 23:50:37 -0400

    fix sql statement

--- bot/utils.py
+++ bot/utils.py
@@ -1,9 +1,11 @@
-from bot import conn
+import sqlite3
+
+conn = sqlite3.connect('../users.db', check_same_thread=False)
 
 
 def create_db_tables():
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS ignored (username text')
+    c.execute('CREATE TABLE IF NOT EXISTS ignored (username text)')
     c.execute('CREATE TABLE IF NOT EXISTS mods (username text)')
 
 
